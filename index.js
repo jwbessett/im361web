08267fc02b082ce23bc5080ebcd5dff44c80f5b8
jwbessett <60371078+jwbessett@users.noreply.github.com>
2020-02-05 17:41:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const readline = require('readline').createInterface({
 readline.question(`Would you like a cookie?`, (response) => {
   if(response == "yes" || response == "y") {
     console.log("Too bad");
-    console.log("I tried to get it to open Rick Astleys Never Gonna Give You Up but couldnt figure it out. Pretend you got Rick Rolled")
+    console.log("I tried to get it to open Rick Astleys Never Gonna Give You Up but couldnt figure it out. Pretend you got Rick Rolled");
   }
   else if(response == "no" || response == "n") {
     console.log("I didnt have one anyway");
