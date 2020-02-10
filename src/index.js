fd9a8b7291cda536e95142bd4d886a6be87d8892
jwbessett <60371078+jwbessett@users.noreply.github.com>
2020-02-10 11:54:43 -0600

    Trying to add div

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import Vue from 'vue/dist/vue.js'
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'i dont like grapefruit',
-    counter: 0
-  }
+
 });
+
+addText() {
+  var text = document.createElement("P");
+  text.innerText = "This will be a new task";
+  document.body.appendChild(text);
+}
 
 window.app = app;
