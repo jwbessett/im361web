29b6629fab0a31616865afe313d505aef72f5cca
jwbessett <60371078+jwbessett@users.noreply.github.com>
2020-02-12 11:47:58 -0600

    HTML markup

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import Vue from 'vue/dist/vue.js'
 var app = new Vue({
   el: '#app',
   data: {
-
+    task1Label: 'Do Laundry',
+    task1Description: 'Take the dirty laundry down to the laundry room and put it in th washer',
+    task1Sub1: 'Wash clothes',
+    task1Sub2: 'Dry clothes',
+    task1Sub3: 'Fold clothes'
+  }
 });
-
-function addText() {
-  var text = document.createElement("P");
-  text.innerText = "This will be a new task";
-  document.body.appendChild(text);
-}
 
 window.app = app;
